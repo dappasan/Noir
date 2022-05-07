140efd5dae6f47848cdb0a379da4532a4f1af25e
Dappa <94505216+ReyyNada@users.noreply.github.com>
2022-05-07 13:08:43 +0700

    Update __main__.py

--- YinsRobot/__main__.py
+++ YinsRobot/__main__.py
@@ -367,7 +367,7 @@ def yins_about_callback(update, context):
             "\n• I can warn users until they reach max warns, with each predefined actions such as ban, mute, kick, etc."
             "\n• I have a note keeping system, blacklists, and even predetermined replies on certain keywords."
             "\n• I check for admins' permissions before executing any command and more stuffs"
-            "\n\n_Yin's licensed under the GNU General Public License v3.0_"
+            "\n\n_ɴᴏɪʀ licensed under the GNU General Public License v3.0_"
             "\n\n Click on button bellow to get basic help for ɴᴏɪʀ.",
             parse_mode=ParseMode.MARKDOWN,
             disable_web_page_preview=True,
