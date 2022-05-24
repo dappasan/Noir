1a24b5a17e7131278d91adfbecb3e640605c756a
Dappa <94505216+ReyyNada@users.noreply.github.com>
2022-05-24 22:52:03 +0700

    Update alive.py

--- YinsRobot/modules/alive.py
+++ YinsRobot/modules/alive.py
@@ -9,16 +9,17 @@ from YinsRobot.events import register
 from YinsRobot import telethn as tbot
 
 
-PHOTO = "https://telegra.ph/file/7e2c64c1258218a2ef62a.jpg"
+PHOTO = "https://telegra.ph/file/4d5859f35db5a7431fbe4.jpg"
 
 @register(pattern=("/alive"))
 async def awake(event):
   TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm ɴᴏɪʀ.** \n\n"
   TEXT += "✨ **I'm Working Properly** \n\n"
   TEXT += f"✨ **My Master : [dappa](https://t.me/mahadappa)** \n\n"
+  TEXT += f"✨ **My Master : [bdrl](https://t.me/bdrlbukan)** \n\n"
   TEXT += f"✨ **Lɪʙʀᴀʀʏ Vᴇʀsɪᴏɴ    :** `{telever}` \n\n"
   TEXT += f"✨ **Tᴇʟᴇᴛʜᴏɴ Vᴇʀsɪᴏɴ   :** `{tlhver}` \n\n"
   TEXT += f"✨ **Pʏʀᴏɢʀᴀᴍ Vᴇʀsɪᴏɴ :** `{pyrover}` \n\n"
   TEXT += "**Thanks For Adding Me Here ✨**"
-  BUTTON = [[Button.url("Help", "https://t.me/NoirPunyaBot?start=help"), Button.url("Channel", "https://t.me/st4yhallal")]]
+  BUTTON = [[Button.url("Help", "https://t.me/@zeatapibot?start=help"), Button.url("Channel", "www.xnxx.com")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=TEXT,  buttons=BUTTON)
