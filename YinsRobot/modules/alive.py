d30030d369491875de05120c6893c31a7fe524b0
Dappa <94505216+ReyyNada@users.noreply.github.com>
2022-05-26 06:29:05 +0700

    Update alive.py

--- YinsRobot/modules/alive.py
+++ YinsRobot/modules/alive.py
@@ -14,12 +14,7 @@ PHOTO = "https://telegra.ph/file/4d5859f35db5a7431fbe4.jpg"
 @register(pattern=("/alive"))
 async def awake(event):
   TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm ɴᴏɪʀ.** \n\n"
-  TEXT += "✨ **I'm Working Properly** \n\n"
-  TEXT += f"✨ **My Master : [dappa](https://t.me/mahadappa)** \n\n"
-  TEXT += f"✨ **My Master : [bdrl](https://t.me/bdrlbukan)** \n\n"
-  TEXT += f"✨ **Lɪʙʀᴀʀʏ Vᴇʀsɪᴏɴ    :** `{telever}` \n\n"
-  TEXT += f"✨ **Tᴇʟᴇᴛʜᴏɴ Vᴇʀsɪᴏɴ   :** `{tlhver}` \n\n"
-  TEXT += f"✨ **Pʏʀᴏɢʀᴀᴍ Vᴇʀsɪᴏɴ :** `{pyrover}` \n\n"
-  TEXT += "**Thanks For Adding Me Here ✨**"
-  BUTTON = [[Button.url("Help", "https://t.me/@zeatapibot?start=help"), Button.url("Channel", "www.xnxx.com")]]
+  TEXT += "✨ **DUA TIGA TUTUP BOTOL BACOT KONTOL** \n\n"
+  
+
   await tbot.send_file(event.chat_id, PHOTO, caption=TEXT,  buttons=BUTTON)
