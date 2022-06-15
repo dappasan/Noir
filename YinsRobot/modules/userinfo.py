f1bd7e23c5ccf8ba80e4be96675caee8d822976a
Bdrl <93611653+Yansaii@users.noreply.github.com>
2022-06-16 03:28:55 +0700

    Update userinfo.py

--- YinsRobot/modules/userinfo.py
+++ YinsRobot/modules/userinfo.py
@@ -445,9 +445,9 @@ def set_about_me(update: Update, context: CallbackContext):
 
 @sudo_plus
 def stats(update: Update, context: CallbackContext):
-    stats = "<b>╔═━「 Current Yins Statistics 」</b>\n" + "\n".join([mod.__stats__() for mod in STATS])
+    stats = "<b>╔═━「 Current Ichiro Statistics 」</b>\n" + "\n".join([mod.__stats__() for mod in STATS])
     result = re.sub(r"(\d+)", r"<code>\1</code>", stats)
-    result += "\n<b>╘═━「 Powered By maha lora </ 」</b>"
+    result += "\n<b>╘═━「 Powered Ichiro/ 」</b>"
     update.effective_message.reply_text(
         result,
         parse_mode=ParseMode.HTML, 
