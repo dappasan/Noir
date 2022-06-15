a4079901e1ba124ad97c82de60f241259fb0c2a2
dappasan <105788745+dappasan@users.noreply.github.com>
2022-06-16 03:28:50 +0700

    .

--- YinsRobot/modules/logomaker.py
+++ YinsRobot/modules/logomaker.py
@@ -113,7 +113,7 @@ async def logo_gen(event):
         await tbot.send_file(
             event.chat_id,
             file=flnme,
-            caption="Logo by [ɴᴏɪʀ](https://t.me/NoirPunyaBot)",
+            caption="Logo by [ɪᴄʜɪʀᴏ](https://t.me/ichitrotapibot)",
             force_document=False,
         )
         os.remove(flnme)
@@ -196,7 +196,7 @@ async def logo_(event):
         await tbot.send_file(
             event.chat_id,
             file=flnme,
-            caption="Logo by [ɴᴏɪʀ](https://t.me/NoirPunyaBot)",
+            caption="Logo by [ɪᴄʜɪʀᴏ](https://t.me/ichitrotapibot)",
             force_document=False,
         )
         os.remove(flnme)
